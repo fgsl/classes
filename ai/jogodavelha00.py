6eae29413ed062e9b6fa589deb64ad0f3f4aed21
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2019-07-12 13:55:12 -0300

    Começa exercício do jogo da velha

---
+++ ai/jogodavelha00.py
@@ -0,0 +1,16 @@
+# jogo da velha
+jogador1 = Jogador()
+jogador2 = Jogador()
+
+tabuleiro = Tabuleiro()
+tabuleiro.escrever()
+
+class Jogador:
+
+class Tabuleiro:
+    def escrever(casa):
+        print "este método deve escrever um símbolo em uma casa do tabuleiro"  
+
+
+
+
